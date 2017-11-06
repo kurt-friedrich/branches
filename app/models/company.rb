fcd6efcbd8133ee15c66ad5f62b9875b34938591
Kurt Friedrich <kurt.is.friedrich@gmail.com>
2017-11-05 21:51:59 -0500

    Add Company model validations

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,7 @@
 class Company < ApplicationRecord
+  validates :name, presence: true,
+                   length: { minimum: 2 }
+
+  validates :plan_level, inclusion: { in: %w(legacy custom basic plus growth
+                                             enterprise) }
 end
