650827a361af3be09b93a81258ce2f8c47936e95
Kurt Friedrich <kurt.is.friedrich@gmail.com>
2017-11-06 01:41:54 -0500

    Add format validation for Lesson name

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,5 +1,6 @@
 class Lesson < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true,
+                   format: { with: /\A[a-zA-Z0-9 ]+\z/ }
 
   belongs_to :company
 end
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Lesson, type: :model do
+  let(:subject) { FactoryBot.build(:lesson) }
+
   # name
   it { should validate_presence_of(:name) }
+  it { should allow_value('Lesson').for(:name) }
+  it { should allow_value('Lesson1').for(:name) }
+  it { should allow_value('Lesson 1').for(:name) }
+  it { should_not allow_value('Le$$on').for(:name) }
 
   # company
   it { should belong_to(:company) }
