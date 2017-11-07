6332c48fd971b8251594bd14d3b66dca26db648f
Kurt Friedrich <kurt.is.friedrich@gmail.com>
2017-11-06 21:17:26 -0500

    Add route to get companies with modern plan level

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   get 'companies/alphabetically'
 
+  get 'companies/with_modern_plan'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
